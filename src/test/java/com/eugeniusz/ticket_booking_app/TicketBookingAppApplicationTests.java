c171f5f03701c58e2ad72e6a01118f7bec0d295b
Eugeniusz <eugenioskye@gmail.com>
2023-05-17 19:32:42 +0200

    Initial commit

---
+++ src/test/java/com/eugeniusz/ticket_booking_app/TicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugeniusz.ticket_booking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
